97b7d67c5f6664aaa4164019c8d4d853234c4149
chloebeirne <g00399850@atu.ie>
2022-10-25 13:32:13 +0100

    Set up a new class called Info

--- src/main/java/ie/atu/week6/ShapeApp.java
+++ src/main/java/ie/atu/week6/ShapeApp.java
@@ -3,5 +3,12 @@ package ie.atu.week6;
 import java.util.Scanner;
 
 public class ShapeApp {
+
+    public static void main(String[] args){
+        //create first instance
+        System.out.println("Enter students name: ");
+        Scanner input = new Scanner(System.in);
+
+    }
 }
 
